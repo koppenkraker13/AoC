e327e8759025f1634c7f399080ce36a4d70abc54
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2021-12-08 20:31:40 +0100

    Cleaning up the code a bit

--- day8/src/Day8Part2.java
+++ day8/src/Day8Part2.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,6 +32,53 @@ public class Day8Part2 {
         return result;
     }
 
+    public static List<String> encryptNumbers(String[] exampleData) {
+        String[] numbers = new String[10];
+        ArrayList<String> length5 = new ArrayList<>();
+        ArrayList<String> length6 = new ArrayList<>();
+        for (String str : exampleData) {
+            switch (str.length()) {
+                case 2:
+                    numbers[1] = sort(str);
+                    break;
+                case 3:
+                    numbers[7] = sort(str);
+                    break;
+                case 4:
+                    numbers[4] = sort(str);
+                    break;
+                case 5:
+                    length5.add(sort(str));
+                    break;
+                case 6:
+                    length6.add(sort(str));
+                    break;
+                case 7:
+                    numbers[8] = sort(str);
+                    break;
+            }
+        }
+        for (String num : length5) {
+            if (isContained(num, numbers[1])) {
+                numbers[3] = sort(num);
+            } else if (isContained(num, subtractString(numbers[4], numbers[1]))) {
+                numbers[5] = sort(num);
+            } else {
+                numbers[2] = sort(num);
+            }
+        }
+        for (String num : length6) {
+            if (isContained(num, numbers[3])) {
+                numbers[9] = sort(num);
+            } else if (isContained(num, subtractString(numbers[8], subtractString(numbers[1], numbers[5])))) {
+                numbers[6] = sort(num);
+            } else {
+                numbers[0] = sort(num);
+            }
+        }
+        return Arrays.asList(numbers);
+    }
+
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("day8/data/day8.txt"))) {
             String line;
@@ -40,56 +86,12 @@ public class Day8Part2 {
             while ((line = br.readLine()) != null) {
                 String[] exampleData = line.split("\\| ")[0].split(" ");
                 String[] questionData = line.split("\\| ")[1].split(" ");
-                String[] numbers = new String[10];
-                ArrayList<String> length5 = new ArrayList<>();
-                ArrayList<String> length6 = new ArrayList<>();
-                for (String str : exampleData) {
-                    switch (str.length()) {
-                        case 2:
-                            numbers[1] = sort(str);
-                            break;
-                        case 3:
-                            numbers[7] = sort(str);
-                            break;
-                        case 4:
-                            numbers[4] = sort(str);
-                            break;
-                        case 5:
-                            length5.add(sort(str));
-                            break;
-                        case 6:
-                            length6.add(sort(str));
-                            break;
-                        case 7:
-                            numbers[8] = sort(str);
-                            break;
-                    }
-                }
-                for (String num : length5) {
-                    if (isContained(num, numbers[1])) {
-                        numbers[3] = sort(num);
-                    } else if (isContained(num, subtractString(numbers[4], numbers[1]))) {
-                        numbers[5] = sort(num);
-                    } else {
-                        numbers[2] = sort(num);
-                    }
-                }
-                for (String num : length6) {
-                    String test = subtractString(numbers[1], numbers[5]);
-                    if (isContained(num, numbers[3])) {
-                        numbers[9] = sort(num);
-                    } else if (isContained(num, subtractString(numbers[8], subtractString(numbers[1], numbers[5])))) {
-                        numbers[6] = sort(num);
-                    } else {
-                        numbers[0] = sort(num);
-                    }
-                }
-                List<String> numbersString = Arrays.asList(numbers);
-                String number = "";
+                List<String> numbersString = encryptNumbers(exampleData);
+                StringBuilder number = new StringBuilder();
                 for (String num : questionData) {
-                    number += numbersString.indexOf(sort(num));
+                    number.append(numbersString.indexOf(sort(num)));
                 }
-                total += Integer.parseInt(number);
+                total += Integer.parseInt(number.toString());
             }
             System.out.println(total);
         } catch (IOException e) {
