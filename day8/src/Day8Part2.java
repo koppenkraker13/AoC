a2dfcd18ff08db7aa46986c898e9e0cd60c662f4
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2021-12-08 18:12:58 +0100

    Finally part2 works as well

---
+++ day8/src/Day8Part2.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day8Part2 {
+    public static String sort(String inputString) {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public static Boolean isContained(String big, String small) {
+        boolean isContained = true;
+        for (char smallChar : small.toCharArray()) {
+            if (big.indexOf(smallChar) == -1) {
+                isContained = false;
+            }
+        }
+        return isContained;
+    }
+
+    public static String subtractString(String first, String second) {
+        String result = first;
+        for (char secondChar : second.toCharArray()) {
+            if (result.indexOf(secondChar) != -1) {
+                result = result.replace(Character.toString(secondChar), "");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("day8/data/day8.txt"))) {
+            String line;
+            int total = 0;
+            while ((line = br.readLine()) != null) {
+                String[] exampleData = line.split("\\| ")[0].split(" ");
+                String[] questionData = line.split("\\| ")[1].split(" ");
+                String[] numbers = new String[10];
+                ArrayList<String> length5 = new ArrayList<>();
+                ArrayList<String> length6 = new ArrayList<>();
+                for (String str : exampleData) {
+                    switch (str.length()) {
+                        case 2:
+                            numbers[1] = sort(str);
+                            break;
+                        case 3:
+                            numbers[7] = sort(str);
+                            break;
+                        case 4:
+                            numbers[4] = sort(str);
+                            break;
+                        case 5:
+                            length5.add(sort(str));
+                            break;
+                        case 6:
+                            length6.add(sort(str));
+                            break;
+                        case 7:
+                            numbers[8] = sort(str);
+                            break;
+                    }
+                }
+                for (String num : length5) {
+                    if (isContained(num, numbers[1])) {
+                        numbers[3] = sort(num);
+                    } else if (isContained(num, subtractString(numbers[4], numbers[1]))) {
+                        numbers[5] = sort(num);
+                    } else {
+                        numbers[2] = sort(num);
+                    }
+                }
+                for (String num : length6) {
+                    String test = subtractString(numbers[1], numbers[5]);
+                    if (isContained(num, numbers[3])) {
+                        numbers[9] = sort(num);
+                    } else if (isContained(num, subtractString(numbers[8], subtractString(numbers[1], numbers[5])))) {
+                        numbers[6] = sort(num);
+                    } else {
+                        numbers[0] = sort(num);
+                    }
+                }
+                List<String> numbersString = Arrays.asList(numbers);
+                String number = "";
+                for (String num : questionData) {
+                    number += numbersString.indexOf(sort(num));
+                }
+                total += Integer.parseInt(number);
+            }
+            System.out.println(total);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
