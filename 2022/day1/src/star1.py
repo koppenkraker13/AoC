265af910ff5e636cfb7ff9411ba70101d1cbf1b6
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2022-12-01 11:08:08 +0100

    Forgot to close the file (Thanks Mark)

--- 2022/day1/src/star1.py
+++ 2022/day1/src/star1.py
@@ -1,12 +1,14 @@
-f = open("../data/data.txt", mode="r")
 elf_list = []
 elf_total = 0
-for line in f:
-    if line == "\n":
-        elf_list.append(elf_total)
-        elf_total = 0
-    else:
-        elf_total += int(line)
+
+with open("../data/data.txt", mode="r") as fp:
+    for line in fp:
+        if line == "\n":
+            elf_list.append(elf_total)
+            elf_total = 0
+        else:
+            elf_total += int(line)
+
 elf_list.append(elf_total)
 elf_list.sort()
 print("res1: " + str(elf_list[-1]))
