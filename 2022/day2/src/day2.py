c233b60af9b095ab2e4de49447453dc7cfe59600
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2022-12-02 12:59:04 +0100

    made it 4 lines

--- 2022/day2/src/day2.py
+++ 2022/day2/src/day2.py
@@ -1,5 +1,4 @@
-opponent, me = ["A", "B", "C"], ["X", "Y", "Z"]
-with open("../data/data.txt", mode="r") as fp:
-    print(sum(map(lambda y: ((me.index(y[1]) - opponent.index(y[0]) + 1) % 3) * 3 + me.index(y[1]) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
+with open("../data/data.txt") as fp:
+    print(sum(map(lambda y: ((ord(y[1]) - ord(y[0]) - 22) % 3) * 3 + ord(y[1]) - 87, map(lambda x: x.replace("\n", "").split(" "), fp))))
     fp.seek(0)
-    print(sum(map(lambda y: (me.index(y[1]) * 3) + ((opponent.index(y[0]) + me.index(y[1]) - 1) % 3) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
+    print(sum(map(lambda y: ((ord(y[1]) - 88) * 3) + ((ord(y[0]) - 65 + ord(y[1]) - 89) % 3) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
