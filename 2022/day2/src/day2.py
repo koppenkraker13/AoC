84a03d51d5e996f37f50c8862b981b57c6062b53
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2022-12-02 12:42:40 +0100

    removed unused variable and fixed spelling mistake

--- 2022/day2/src/day2.py
+++ 2022/day2/src/day2.py
@@ -1,6 +1,5 @@
-oponent, me, score = ["A", "B", "C"], ["X", "Y", "Z"], 0
+opponent, me = ["A", "B", "C"], ["X", "Y", "Z"]
 with open("../data/data.txt", mode="r") as fp:
-    print(sum(map(lambda y: ((me.index(y[1]) - oponent.index(y[0]) + 1) % 3) * 3 + me.index(y[1]) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
+    print(sum(map(lambda y: ((me.index(y[1]) - opponent.index(y[0]) + 1) % 3) * 3 + me.index(y[1]) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
     fp.seek(0)
-    print(sum(map(lambda y: (me.index(y[1]) * 3) + ((oponent.index(y[0]) + me.index(y[1]) - 1) % 3) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
-
+    print(sum(map(lambda y: (me.index(y[1]) * 3) + ((opponent.index(y[0]) + me.index(y[1]) - 1) % 3) + 1, map(lambda x: x.replace("\n", "").split(" "), fp))))
