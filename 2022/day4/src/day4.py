b63881a9be8bf9c8d47b47246fcb843a4de08311
Jesse Snoijer <j.h.snoijer@student.utwente.nl>
2022-12-04 10:36:14 +0100

    Improved complexity

--- 2022/day4/src/day4.py
+++ 2022/day4/src/day4.py
@@ -1,33 +1,15 @@
-solution1 = 0
+solution1, solution2 = 0, 0
 with open("../data/data.txt") as fp:
     for line in fp:
         splitted = line.split(",")
-        left = splitted[0].split("-")
-        right = splitted[1].replace("\n", "").split("-")
-        if int(left[0]) == int(right[0]) or int(left[1]) == int(right[1]):
+        left = list(map(lambda x: int(x), splitted[0].split("-")))
+        right = list(map(lambda x: int(x), splitted[1].replace("\n", "").split("-")))
+
+        if (left[0] == right[0] or left[1] == right[1]) or (left[0] < right[0] and left[1] > right[1]) or (left[0] > right[0] and left[1] < right[1]):
             solution1 += 1
-            continue
-        if int(left[0]) < int(right[0]) and int(left[1]) > int(right[1]):
-            solution1 += 1
-            continue
-        if int(left[0]) > int(right[0]) and int(left[1]) < int(right[1]):
-            solution1 += 1
+
+        if (left[0] <= right[0] <= left[1]) or (left[0] <= right[1] <= left[1]) or (right[0] < left[0] and right[1] > left[1]):
+            solution2 += 1
 
 print(solution1)
-
-solution2 = 0
-with open("../data/data.txt") as fp:
-    for line in fp:
-        splitted = line.split(",")
-        left = splitted[0].split("-")
-        right = splitted[1].replace("\n", "").split("-")
-        if int(left[0]) <= int(right[0]) <= int(left[1]):
-            solution2 += 1
-            continue
-        if int(left[0]) <= int(right[1]) <= int(left[1]):
-            solution2 += 1
-            continue
-        if int(right[0]) < int(left[0]) and int(right[1]) > int(left[1]):
-            solution2 += 1
-
 print(solution2)
