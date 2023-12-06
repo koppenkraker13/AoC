b6aef4a48dff4a1342f74be92735e654dfeeaf30
koppenkraker13 <j.h.snoijer@student.utwente.nl>
2023-12-06 12:19:51 +0100

    Day6 Fewer Lines!

--- 2023/day6/solution.py
+++ 2023/day6/solution.py
@@ -1,20 +1,10 @@
+import math
 import re
 
 with open('input') as f:
-    d = re.compile("(\\d+)")
-    read = f.read().splitlines()
-    lines = [[int(x) for x in d.findall(line)] for line in read]
-    linesSol2 = [int(''.join(d.findall(line))) for line in read]
-    sol1, sol2 = 1, 0
-    for i in range(len(lines[0])):
-        beaten = 0
-        for hold in range(1, lines[0][i]):
-            if hold * (lines[0][i]-hold) > lines[1][i]:
-                beaten += 1
-        sol1 *= beaten
-    for hold in range(1, linesSol2[0]):
-        if hold * (linesSol2[0]-hold) > linesSol2[1]:
-            sol2 += 1
-
+    lines = [[int(x) for x in re.compile("(\\d+)").findall(line)] for line in f.read().splitlines()]
+    linesSol2 = [int(''.join([str(x) for x in line])) for line in lines]
+    sol1 = math.prod(sum(1 for hold in range(1, lines[0][i]) if hold * (lines[0][i] - hold) > lines[1][i]) for i in range(len(lines[0])))
+    sol2 = sum(1 for hold in range(1, linesSol2[0]) if hold * (linesSol2[0] - hold) > linesSol2[1])
     print(f'solution1: {sol1}')
     print(f'solution2: {sol2}')
